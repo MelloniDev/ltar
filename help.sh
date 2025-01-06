6023cca027d559ae2a10b050c1be80b4615bc1f4
Nils Hollenstein <nils.hollenstein@gmail.com>
2025-01-06 15:02:08 +0100

    help.sh written

--- help.sh
+++ help.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
 help-prompt(){
-
+    echo "Aufruf: ./ltar [OPTION]... [DATEI/ORDNER]
+    Verschlüsselung und Komprimierung von Dateien oder Verzeichnissen mithilfe von LUKS
+      -j, --bzip2       Archiv durch bzip2 filtern
+      -J, --xz          Archiv durch xz filtern 
+          --lzip        Archiv durch lzip filtern
+          --lzop        Archiv durch lzop filtern
+      -h, --help        Gibt Hilfe zum Tool aus
+      -q, --quiet       Unterdrückt Ausgabe von Informationen
+    "
 }
+
+help-prompt
