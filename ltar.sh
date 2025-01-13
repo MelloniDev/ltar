8f1fcc9c72cecc6352fe95744eafa80b2f109b72
MelloniDev <>
2025-01-13 15:16:45 +0100

    get password for luks

--- ltar.sh
+++ ltar.sh
@@ -4,13 +4,29 @@ source ./src/help.sh
 #source ./src/handleParams.sh
 source ./src/getFilesSize.sh
 
-
 tempDir="./testing"
 
 files=("./testing/files")
 output="./testing/test.ltar"
 verbose=0
 compression="none"
+action="create"
+
+
+
+echo -n "Please enter your password: "
+read -s luksPassword
+echo ""
+
+echo -n "Please repeat your password: "
+read -s luksPasswordVerify
+echo ""
+
+if [[ "$luksPassword" == "$luksPasswordVerify" ]]; then
+    echo "$luksPassword"
+else
+    echo "Passwords didn't match"
+fi
 
 
 # handleParams $@
