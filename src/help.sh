99e330b441aacfc1b01c7318eafd4d343e819caf
Nils Hollenstein <nils.hollenstein@gmail.com>
2025-01-20 13:48:09 +0100

    Help-Output korrigiert

--- src/help.sh
+++ src/help.sh
@@ -2,8 +2,8 @@
 help_prompt(){
     echo "Aufruf: ./ltar [OPTION]... [DATEI/ORDNER]
     Verschlüsselung und Komprimierung von Dateien oder Verzeichnissen mithilfe von LUKS
-      -j, --bzip2       Archiv durch bzip2 komprimieren
-      -J, --xz          Archiv durch xz komprimieren 
+      -j, --bzip2       Archiv mit bzip2 komprimieren
+      -J, --xz          Archiv mit xz komprimieren 
       -h, --help        Gibt Hilfe zum Tool aus
       -q, --quiet       Unterdrückt Ausgabe von Informationen
       -x                Die Datei wird extrahiert
